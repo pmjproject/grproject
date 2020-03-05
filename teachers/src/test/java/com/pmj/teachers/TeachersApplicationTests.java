18c6a0a238a37968d55f76e9a8d3e3eff75fbbb7
pmjproject <jayakodymangalya@gmail.com>
2020-03-05 22:51:26 +0530

    Add files via upload

---
+++ teachers/src/test/java/com/pmj/teachers/TeachersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmj.teachers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
